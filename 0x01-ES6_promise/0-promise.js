714ae6ea67fa9928a4059c62fb4b7f96e412586c
Nenenj <nnennanjoku08@gmail.com>
2024-06-26 21:21:43 +0100

    update

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,4 @@
+/* eslint-disable */
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // Simulate an API response
-    resolve('API response');
-  });
+	return new Promise((resolve, reject) => {});
 }
